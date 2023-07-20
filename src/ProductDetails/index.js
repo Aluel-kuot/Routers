3f06e5090bf222f4b2e85a8328b79fc60ce39cc1
Aluel-kuot <maryaluelkuot@gmail.com>
2023-07-20 09:28:03 +0300

    routers

--- src/ProductDetails/index.js
+++ src/ProductDetails/index.js
@@ -1,66 +1,48 @@
-import React,{useState, useEffect} from "react";
-import { Link} from "react-router-dom";
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import './style.css'
 
 
+const ProductDetails = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
 
-
-const ProductDetails=()=>{
-    const [products,setProducts] = useState([]);
-    const [loading,setLoading] = useState ([false]);
-  //   const [searchParams] = useSearchParams();
-  // const productId = searchParams.get('productId');
-    useEffect(()=>{
-    (async()=>{
-    await getProducts();
-})()
-    }, [])
-    console.log({products});
-
-    const getProducts=async()=>{
-        try{
-            setLoading(true)
-            const response=await fetch('https://dummyjson.com/products')
-            const result =await response .json();
-            setProducts(result.products);
-            setLoading(false);
-
-        }
-        catch(error){
-            console.log(error.message);
-        }
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await fetch(`https://dummyjson.com/products/${productId}`);
+        const result = await response.json();
+        setProduct(result);
+      } catch (error) {
+        console.log(error.message);
+      }
     };
-    
-        return (
-            <div className="list">
-              <h1>Details</h1>
-              <div className="products">
-                {products.map((item) => (
-                  <div key={item.id} className="item">
-                    <img src={item.thumbnail} className="image" />
-                    <div className="details">
-                      <h2 className="title">{item.title}</h2>
-                      <h2 className="title">{item.id}</h2>
-                      <p className="price">{item.price}</p>
-                      <p className="discount">{item.discountPercentage}</p>
-                      <p className="discount">{item.rating}</p>
-                      <p className="discount">{item.discription}</p>
-                      <p className="discount">{item.stock}</p>
-                      <p className="discount">{item.category}</p>
-                      <p className="discount">{item.brand}</p>
 
-                      
-v
-                    </div>
-                    <button className="view">Add to cart</button>
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        };
-        
-        export default ProductDetails;
-        
-        
-     
+    getProduct();
+  }, [productId]);
+
+  if (!product) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="product-details">
+      <h1>Product Details</h1>
+      <div className="details">
+        <img src={product.thumbnail} className="image" />
+        <h2 className="title">{product.title}</h2>
+        <p className="price">{product.price}</p>
+        <p className="discount">{product.discountPercentage}</p>
+        <p className="rating">{product.rating}</p>
+        <p className="description">{product.description}</p>
+        <p className="stock">{product.stock}</p>
+        <p className="category">{product.category}</p>
+        <p className="brand">{product.brand}</p>
+        <button className="add-to-cart">Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/Products/index.js
+++ src/Products/index.js
@@ -1,60 +1,55 @@
-import React,{useState, useEffect} from "react";
-import { Link} from "react-router-dom";
+
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import './style.css'
 
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-
-
-const Products=()=>{
-    const [products,setProducts] = useState([]);
-    const [loading,setLoading] = useState ([false]);
-  //   const [searchParams] = useSearchParams();
-  // const productId = searchParams.get('productId');
-    useEffect(()=>{
-    (async()=>{
-    await getProducts();
-})()
-    }, [])
-    console.log({products});
-
-    const getProducts=async()=>{
-        try{
-            setLoading(true)
-            const response=await fetch('https://dummyjson.com/products')
-            const result =await response .json();
-            setProducts(result.products);
-            setLoading(false);
-
-        }
-        catch(error){
-            console.log(error.message);
-        }
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const result = await response.json();
+        setProducts(result.products);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
     };
-    
-        if(loading){
-            return <h2>Loading...</h2>
-        }
-        return (
-            <div className="list">
-              <h1>All products</h1>
-              <div className="products">
-                {products.map((item) => (
-                  <div key={item.id} className="item">
-                    <img src={item.thumbnail} className="image" />
-                    <div className="details">
-                      <h2 className="title">{item.title}</h2>
-                      <p className="price">{item.price}</p>
-                      <p className="discount">{item.discountPercentage}</p>
-                    </div>
-                    <button className="view">View Details</button>
-                  </div>
-                ))}
-              </div>
+
+    getProducts();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="list">
+      <h1>All products</h1>
+      <div className="products">
+        {products.map((item) => (
+          <div key={item.id} className="item">
+            <img src={item.thumbnail} className="image" alt={item.title} />
+            <div className="details">
+              <h2 className="title">{item.title}</h2>
+              <p className="price">{item.price}</p>
+              <p className="discount">{item.discountPercentage}</p>
             </div>
-          );
-        };
-        
-        export default Products;
+            <button className="view" onClick={() => navigate(`/products/${item.id}`)}>View Details</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
+
         
         
      
